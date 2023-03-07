dec87f606ef1c4bbd0b4c306db8c2f26ef0b0659
Mutlu <mutluturedi@gmail.com>
2023-03-07 09:51:00 +0100

    Fatih Branch Pull et

--- src/gun17/Asalsayi.java
+++ src/gun17/Asalsayi.java
@@ -2,17 +2,20 @@ package gun17;
 
 public class Asalsayi {
     public static void main(String[] args) {
-        int numara = 45;
+       // int numara = 45;
         // asal sayimi
         // 1 ve kendisinden baska sayiya bolunuyor mu
         //kalani kontrol etmek
-        int i = 2;
+     //   int i = 2;
 
         // Birinici Deneme
 
         // Ikinci Deneme
 
         // 3. Degisiklik
+
+        // 4. Degisiklik
+
 
 
     }
